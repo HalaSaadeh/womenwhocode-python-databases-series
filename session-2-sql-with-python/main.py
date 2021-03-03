75e0acd9e07920ac9d28afe518b79152a2d7a37c
HalaSaadeh <halagsaadeh@gmail.com>
2021-03-03 19:41:33 +0100

    added session 2 materials

--- session-2-sql-with-python/main.py
+++ session-2-sql-with-python/main.py
@@ -1,1 +1,23 @@
 import sqlite3
+
+conn = sqlite3.connect('chinook.db')
+cur = conn.cursor()
+
+# CREATE DATA
+sqlstatement = CREATE TABLE IF NOT EXISTS CREDITCARD(cardID CHAR(4) NOT NULL, accountID INT NOT NULL, PRIMARY KEY(cardID), FOREIGN KEY accountID REFERENCES BANKACCOUNT(accountID))
+cur.execute(sqlstatement)
+
+# READ DATA
+sqlstatement = "SELECT * FROM albums, artists WHERE artists.name = \"AC/DC\" AND albums.ArtistId=artists.ArtistId"
+for row in cur.execute(sqlstatement):
+    print(str(row))
+    
+# UPDATE DATA
+sqlstatement = UPDATE PLAYLISTS set Name = "Music Play" WHERE Name = "Music"
+cur.execute(sqlstatement)
+
+# DELETE DATA
+sqlstatement = DELETE FROM invoices WHERE BillingCountry = "France"
+cur.execute(sqlstatement)
+
+conn.close()
